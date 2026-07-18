3546ad51ff5fa2801629c6db2de61615b6319218
Meghana290607 <meghanalakshmisetty@gmail.com>
2026-07-18 19:11:02 +0530

    Create Analysis_Queries.sql

---
+++ SQL/Analysis_Queries.sql
@@ -0,0 +1,4 @@
+select *  from evindia;
+select * from electriccardata_clean;
+select * from electriccardata_clean where powerTrain = 'AWD';
+select * from electriccardata_clean where powerTrain = 'AWD' and BodyStyle = 'Sedan';
