2b736dae94ebbb6edc6e6a979030dcdd34ba7725
Meghana290607 <meghanalakshmisetty@gmail.com>
2026-07-18 19:10:28 +0530

    Create Create_Tables.sql

---
+++ SQL/Create_Tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE electric_cars (
+    Brand VARCHAR(50),
+    Model VARCHAR(100),
+    AccelSec DECIMAL(4,1),
+    TopSpeed_KmH INT,
+    Range_Km INT,
+    Efficiency_WhKm INT,
+    FastCharge_KmH INT,
+    RapidCharge VARCHAR(10),
+    PowerTrain VARCHAR(20),
+    PlugType VARCHAR(50),
+    BodyStyle VARCHAR(30),
+    Segment CHAR(1),
+    Seats INT,
+    PriceEuro INT
+);
